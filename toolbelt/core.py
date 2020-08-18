122bb42c0094040305c81169a7bbaf00975d5477
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2020-08-18 10:47:36 +0900

    add core module

---
+++ toolbelt/core.py
@@ -0,0 +1,7 @@
+def normalize(text: str) -> str:
+    return " ".join(text.strip().lower().split())
+
+
+def chunk(items, size):
+    for i in range(0, len(items), size):
+        yield items[i:i + size]
