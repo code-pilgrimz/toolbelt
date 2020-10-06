4d18001bdb2d21da929d59b780a438594280123e
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2020-10-06 11:31:10 +0900

    add tests

---
+++ tests/test_core.py
@@ -0,0 +1,9 @@
+from toolbelt.core import normalize, chunk
+
+
+def test_normalize():
+    assert normalize("  Hello   World ") == "hello world"
+
+
+def test_chunk():
+    assert list(chunk([1, 2, 3, 4, 5], 2)) == [[1, 2], [3, 4], [5]]
